c20bf4b848b86ca6c61678a7c5d2412f94960637
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-11-03 17:08:53 +0100

    Added loading of markers from localStorage if there's data already stored

--- main.js
+++ main.js
@@ -4,8 +4,25 @@ import * as MapSettings from './Map'
 
 const map = L.map('map')
 
-Api.getNuclearPowerPlantsData()
-.then(data => {
-	MapSettings.initMap(map)
+MapSettings.initMap(map)
+
+if (localStorage.getItem('nuclearPowerPlantsData') === null) {
+	Api.getNuclearPowerPlantsData()
+	.then(data => {
+		console.log('%c Loaded markers data from API call ', 'color: white; background-color: green')
+
+		// Save data to localStorage as JSON
+		localStorage.setItem('nuclearPowerPlantsData', JSON.stringify(data))
+
+		// Render markers on the map
+		MapSettings.setMap(map, data)
+	})
+} else {
+	console.log('%c Loaded markers data from localStorage ', 'color: white; background-color: green')
+
+	// Get data as JSON
+	const data = JSON.parse(localStorage.getItem('nuclearPowerPlantsData'))
+
+	// Render markers on the map
 	MapSettings.setMap(map, data)
-})
+}
