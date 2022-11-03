73f4139ad843736b62c58dd2deb9ae9b1f656d92
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-11-03 17:36:43 +0100

    Added refresh functionality to the navbar

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import './anim.css'
+import './Navbar'
 import * as Api from './Api'
 import * as MapSettings from './Map'
 
-const map = L.map('map')
+let map = L.map('map')
 
 MapSettings.initMap(map)
 
+// Check if there's already data stored in localStorage before making an API call
 if (localStorage.getItem('nuclearPowerPlantsData') === null) {
 	Api.getNuclearPowerPlantsData()
 	.then(data => {
@@ -26,3 +28,36 @@ if (localStorage.getItem('nuclearPowerPlantsData') === null) {
 	// Render markers on the map
 	MapSettings.setMap(map, data)
 }
+
+
+// NAVBAR
+const refreshBtn = document.getElementById('refreshBtn')
+const refreshIcon = document.getElementById('refreshIcon')
+
+refreshBtn.addEventListener('click', () => {
+    if (!refreshIcon.classList.contains('rotate-icon')) {
+        refreshIcon.classList.add('rotate-icon')
+
+        // Make call to API to update the data present in LocalStorage, then update the map view
+        Api.getNuclearPowerPlantsData()
+        .then(data => {
+            map.remove()
+
+			map = L.map('map')
+
+            MapSettings.initMap(map)
+
+            console.log('%c Map was refreshed and the markers in localStorage updated ', 'color: white; background-color: green')
+
+            // Save data to localStorage as JSON
+            localStorage.setItem('nuclearPowerPlantsData', JSON.stringify(data))
+
+            // Render markers on the map
+            MapSettings.setMap(map, data)
+        })
+    }
+
+    setTimeout(() => {
+        refreshIcon.classList.remove('rotate-icon')
+    }, 1000)
+})
